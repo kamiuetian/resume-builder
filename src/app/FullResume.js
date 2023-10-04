f19c59482ffdd80865a0e9ca67e12a73a0198127
kamiuetian <kami_se01@live.com>
2023-10-04 11:33:16 +0500

    Update FullResume.js

--- src/app/FullResume.js
+++ src/app/FullResume.js
@@ -1,129 +1,130 @@
-export const resumeFull={
-    "resume": {
-        "profile": {
-            "name": "DANI MARTINEZ",
-            "summary": "A strong team player experienced in maintaining working relationships with\nco-workers and supervisors by applying excellent organizational and management skills to help report client records to relevant departments daily.",
-            "email": "info@instantjobcv.com",
-            "phone": "+1 315-484-9321",
-            "location": "Buckhannan, NY",
-            "url": "www.instantjobcv,com"
-        },
-        "workExperiences": [
-            {
-                "company": "XYZ Company Name",
-                "jobTitle": "Customer Service Support",
-                "date": "November 2020 – Present",
-                "descriptions": [
-                    "To act as a communication bridge between customers and various departments. o Ensure all transactions are marked and recorded.  ",
-                    "Solve customer’s issues timely and efficiently, through cross departmental communication.",
-                    "Actively resolve problem and give solutions for customers, and communicate them effectively"
-                ]
-            },
-            {
-                "company": "XYZ Company Name",
-                "jobTitle": "Secretary Assistant",
-                "date": "July 2018 - September 2019",
-                "descriptions": [
-                    "Reconcile bank statements at the end of each and generate reports Manage travel arrangements.",
-                    " Write daily report on data from suppliers abroad and prepare samples for outstation sales.  ",
-                    "Perform general office duties such as filing, answering phone calls, handling routine correspondences and replying emails.  ",
-                    "Ensure that all supplier related contracts comply with company guidelines a Compile meeting documentation and distribute accordingly"
-                ]
-            }
-        ],
-        "educations": [
-            {
-                "school": "XYZ University Name",
-                "degree": "Bachelors in Business Administration",
-                "gpa": "3.98",
-                "date": "July 2013 – June 2016",
-                "descriptions": [
-                    "1st position for Stock Trading Competition by Millennium Penata Future 2016.",
-                    "Working as Teacher Assistant in this university and teach many of the core subjects.",
-                    "Our core courses includes Marketing and Operations Research, Manufacture, Planning & Control, Security Analysis, Commercial Bank Management, Financial Modelling Using Spreadsheets."
-                ]
-            }
-        ],
-        "projects": [
-            {
-                "project": "HRIS System",
-                "date": "October 2022",
-                "descriptions": [
-                    "Information technology is expected to drive Human Resource (HR)’s transition from a focus on Human Resource Management (HRM) to Strategic Human Resource Management (SHRM). This strategic role not only adds a valuable dimension to the HR function, but also changes the competencies that define HR professional and practitioner success."
-                ]
-            }
-        ],
-        "skills": {
-            "featuredSkills": [
-                {
-                    "skill": "",
-                    "rating": 4
-                },
-                {
-                    "skill": "",
-                    "rating": 4
-                },
-                {
-                    "skill": "",
-                    "rating": 4
-                },
-                {
-                    "skill": "",
-                    "rating": 4
-                },
-                {
-                    "skill": "",
-                    "rating": 4
-                },
-                {
-                    "skill": "",
-                    "rating": 4
-                }
-            ],
-            "descriptions": [
-                "Competent in use of MS Office.",
-                "Note taking during meetings.",
-                "Highly organized & efficient.",
-                "Salesforce website.",
-                "Adobe Photoshop & internet browsers.",
-                "Effective competitive analysis."
-            ]
-        },
-        "custom": {
-            "descriptions": []
-        }
+export const resumeFull = {
+  resume: {
+    profile: {
+      name: "DANI MARTINEZ",
+      summary:
+        "A strong team player experienced in maintaining working relationships with\nco-workers and supervisors by applying excellent organizational and management skills to help report client records to relevant departments daily.",
+      email: "info@instantjobcv.com",
+      phone: "+1 315-484-9321",
+      location: "Buckhannan, NY",
+      url: "www.instantjobcv,com",
     },
-    "settings": {
-        "themeColor": "#38bdf8",
-        "fontFamily": "Roboto",
-        "fontSize": "11",
-        "documentSize": "Letter",
-        "formToShow": {
-            "workExperiences": true,
-            "educations": true,
-            "projects": true,
-            "skills": true,
-            "custom": false
-        },
-        "formToHeading": {
-            "workExperiences": "WORK EXPERIENCE",
-            "educations": "EDUCATION",
-            "projects": "PROJECT",
-            "skills": "SKILLS",
-            "custom": "CUSTOM SECTION"
-        },
-        "formsOrder": [
-            "workExperiences",
-            "educations",
-            "projects",
-            "skills",
-            "custom"
+    workExperiences: [
+      {
+        company: "XYZ Company Name",
+        jobTitle: "Customer Service Support",
+        date: "November 2020 – Present",
+        descriptions: [
+          "To act as a communication bridge between customers and various departments. o Ensure all transactions are marked and recorded.  ",
+          "Solve customer’s issues timely and efficiently, through cross departmental communication.",
+          "Actively resolve problem and give solutions for customers, and communicate them effectively",
         ],
-        "showBulletPoints": {
-            "educations": true,
-            "projects": true,
-            "skills": true,
-            "custom": true
-        }
-    }
-}
+      },
+      {
+        company: "XYZ Company Name",
+        jobTitle: "Secretary Assistant",
+        date: "July 2018 - September 2019",
+        descriptions: [
+          "Reconcile bank statements at the end of each and generate reports Manage travel arrangements.",
+          " Write daily report on data from suppliers abroad and prepare samples for outstation sales.  ",
+          "Perform general office duties such as filing, answering phone calls, handling routine correspondences and replying emails.  ",
+          "Ensure that all supplier related contracts comply with company guidelines a Compile meeting documentation and distribute accordingly",
+        ],
+      },
+    ],
+    educations: [
+      {
+        school: "XYZ University Name",
+        degree: "Bachelors in Business Administration",
+        gpa: "3.98",
+        date: "July 2013 – June 2016",
+        descriptions: [
+          "1st position for Stock Trading Competition by Millennium Penata Future 2016.",
+          "Working as Teacher Assistant in this university and teach many of the core subjects.",
+          "Our core courses includes Marketing and Operations Research, Manufacture, Planning & Control, Security Analysis, Commercial Bank Management, Financial Modelling Using Spreadsheets.",
+        ],
+      },
+    ],
+    projects: [
+      {
+        project: "HRIS System",
+        date: "October 2022",
+        descriptions: [
+          "Information technology is expected to drive Human Resource (HR)’s transition from a focus on Human Resource Management (HRM) to Strategic Human Resource Management (SHRM). This strategic role not only adds a valuable dimension to the HR function, but also changes the competencies that define HR professional and practitioner success.",
+        ],
+      },
+    ],
+    skills: {
+      featuredSkills: [
+        {
+          skill: "",
+          rating: 4,
+        },
+        {
+          skill: "",
+          rating: 4,
+        },
+        {
+          skill: "",
+          rating: 4,
+        },
+        {
+          skill: "",
+          rating: 4,
+        },
+        {
+          skill: "",
+          rating: 4,
+        },
+        {
+          skill: "",
+          rating: 4,
+        },
+      ],
+      descriptions: [
+        "Competent in use of MS Office.",
+        "Note taking during meetings.",
+        "Highly organized & efficient.",
+        "Salesforce website.",
+        "Adobe Photoshop & internet browsers.",
+        "Effective competitive analysis.",
+      ],
+    },
+    custom: {
+      descriptions: [],
+    },
+  },
+  settings: {
+    themeColor: "#000080",
+    fontFamily: "Roboto",
+    fontSize: "11",
+    documentSize: "Letter",
+    formToShow: {
+      workExperiences: true,
+      educations: true,
+      projects: true,
+      skills: true,
+      custom: false,
+    },
+    formToHeading: {
+      workExperiences: "WORK EXPERIENCE",
+      educations: "EDUCATION",
+      projects: "PROJECT",
+      skills: "SKILLS",
+      custom: "CUSTOM SECTION",
+    },
+    formsOrder: [
+      "workExperiences",
+      "educations",
+      "projects",
+      "skills",
+      "custom",
+    ],
+    showBulletPoints: {
+      educations: true,
+      projects: true,
+      skills: true,
+      custom: true,
+    },
+  },
+};
